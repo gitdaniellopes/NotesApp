b29c9913ad6fb393d7921ee2214a53915e74fe0d
Daniel Lopes <danieljfsantos@gmail.com>
2022-06-28 12:06:05 -0300

    organizando pastas e cores

--- NotesApp/app/src/main/java/br/com/notesapp/ui/theme/Theme.kt
+++ NotesApp/app/src/main/java/br/com/notesapp/ui/theme/Theme.kt
@@ -5,11 +5,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
 )
 
 private val LightColorPalette = lightColors(
@@ -29,14 +32,8 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun NotesAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = DarkColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
