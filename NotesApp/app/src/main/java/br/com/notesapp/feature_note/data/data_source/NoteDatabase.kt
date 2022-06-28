33d30960c3db43b2e1da31297d2958464436c0a5
Daniel Lopes <danieljfsantos@gmail.com>
2022-06-28 12:20:39 -0300

    configurando database

---
+++ NotesApp/app/src/main/java/br/com/notesapp/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,15 @@
+package br.com.notesapp.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import br.com.notesapp.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+}
